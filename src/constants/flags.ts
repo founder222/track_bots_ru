223304a3a0fac9adbf29da40e1dcc62c9220cd28
founder222 <tech344445@gmail.com>
2025-08-09 20:58:29 +0300

    Delete src directory

--- src/constants/flags.ts
+++
@@ -1,8 +0,0 @@
-import { WalletWithUsers } from '../types/swap-types'
-
-export const userExpectingWalletAddress: { [key: number]: boolean } = {}
-export const userExpectingDonation: { [key: number]: boolean } = {}
-export const userExpectingGroupId: { [key: number]: boolean } = {}
-export const walletsToTrack: WalletWithUsers[] = []
-
-export const cachedUsdSolPrice = ''
