5ce49ff4f8352d2d832b5e1508f8403ab7726225
founder222 <tech344445@gmail.com>
2025-08-09 12:33:08 +0300

    Update bot-menus.ts

--- src/config/bot-menus.ts
+++ src/config/bot-menus.ts
@@ -7,24 +7,24 @@ export const START_MENU: InlineKeyboardMarkup = {
   inline_keyboard: [
     // [{ text: '🌟 Buy Limited-Time Offer', callback_data: 'buy_promotion' }],
     [
-      { text: 'Add', callback_data: 'add' },
-      { text: 'Manage', callback_data: 'manage' },
+      { text: 'Добавить', callback_data: 'add' },
+      { text: 'Управлять', callback_data: 'manage' },
     ],
     [
-      { text: 'My Wallet', callback_data: 'my_wallet' },
-      { text: 'Donate', callback_data: 'donate' },
-      { text: 'Settings', callback_data: 'settings' },
+      { text: 'Мой кошелек', callback_data: 'my_wallet' },
+      { text: 'Донат', callback_data: 'donate' },
+      { text: 'Настройки', callback_data: 'settings' },
     ],
     [
-      { text: 'Groups', callback_data: 'groups' },
-      { text: 'Help', callback_data: 'help' },
+      { text: 'Группы', callback_data: 'groups' },
+      { text: 'Помощь', callback_data: 'help' },
     ],
-    [{ text: 'Upgrade', callback_data: 'upgrade' }],
+    [{ text: 'Обновить план', callback_data: 'upgrade' }],
   ],
 }
 
 export const SUB_MENU: InlineKeyboardMarkup = {
-  inline_keyboard: [[{ text: '🔙 Back', callback_data: 'back_to_main_menu' }]],
+  inline_keyboard: [[{ text: '🔙 Назад', callback_data: 'back_to_main_menu' }]],
 }
 
 export const createTxSubMenu = (tokenSymbol: string, tokenMint: string) => {
@@ -32,7 +32,7 @@ export const createTxSubMenu = (tokenSymbol: string, tokenMint: string) => {
     inline_keyboard: [
       [
         {
-          text: `🐴 Buy on Trojan: ${tokenSymbol}`,
+          text: `🐴 Купить на Trojan: ${tokenSymbol}`,
           url: `https://t.me/solana_trojanbot?start=r-handicatbt-${tokenMint}`,
         },
       ],
@@ -58,11 +58,11 @@ export const createTxSubMenu = (tokenSymbol: string, tokenMint: string) => {
 export const MANAGE_SUB_MENU: InlineKeyboardMarkup = {
   inline_keyboard: [
     [
-      { text: 'Add', callback_data: 'add' },
-      { text: '🗑️ Delete', callback_data: 'delete' },
+      { text: 'Добавить', callback_data: 'add' },
+      { text: '🗑️ Удалить', callback_data: 'delete' },
     ],
 
-    [{ text: '🔙 Back', callback_data: 'back_to_main_menu' }],
+    [{ text: '🔙 Назад', callback_data: 'back_to_main_menu' }],
   ],
 }
 
@@ -70,24 +70,24 @@ export const UPGRADE_PLAN_SUB_MENU: InlineKeyboardMarkup = {
   inline_keyboard: [
     [
       {
-        text: `BUY HOBBY ${HOBBY_PLAN_FEE / 1e9} SOL/m`,
+        text: `КУПИТЬ HOBBY ${HOBBY_PLAN_FEE / 1e9} SOL/мес`,
         callback_data: 'upgrade_hobby',
       },
     ],
     [
       {
-        text: `BUY PRO ${PRO_PLAN_FEE / 1e9} SOL/m`,
+        text: `КУПИТЬ PRO ${PRO_PLAN_FEE / 1e9} SOL/мес`,
         callback_data: 'upgrade_pro',
       },
     ],
     [
       {
-        text: `BUY WHALE ${WHALE_PLAN_FEE / 1e9} SOL/m`,
+        text: `КУПИТЬ WHALE ${WHALE_PLAN_FEE / 1e9} SOL/мес`,
         callback_data: 'upgrade_whale',
       },
     ],
 
-    [{ text: '🔙 Back', callback_data: 'back_to_main_menu' }],
+    [{ text: '🔙 Назад', callback_data: 'back_to_main_menu' }],
   ],
 }
 
@@ -98,21 +98,21 @@ export const DONATE_MENU: InlineKeyboardMarkup = {
     [{ text: `💪 ${1.0} SOL`, callback_data: 'donate_action_1.0' }],
     [{ text: `🗿 ${5.0} SOL`, callback_data: 'donate_action_5.0' }],
     [{ text: `🔥 ${10.0} SOL`, callback_data: 'donate_action_10.0' }],
-    [{ text: '🔙 Back', callback_data: 'back_to_main_menu' }],
+    [{ text: '🔙 Назад', callback_data: 'back_to_main_menu' }],
   ],
 }
 
 export const SUGGEST_UPGRADE_SUBMENU: InlineKeyboardMarkup = {
   inline_keyboard: [
-    [{ text: 'Upgrade', callback_data: 'upgrade' }],
-    [{ text: '🔙 Back', callback_data: 'back_to_main_menu' }],
+    [{ text: 'Обновить план', callback_data: 'upgrade' }],
+    [{ text: '🔙 Назад', callback_data: 'back_to_main_menu' }],
   ],
 }
 
 export const INSUFFICIENT_BALANCE_SUB_MENU: InlineKeyboardMarkup = {
   inline_keyboard: [
-    [{ text: '😺 Your Bot Wallet', callback_data: 'my_wallet' }],
-    [{ text: '🔙 Back', callback_data: 'back_to_main_menu' }],
+    [{ text: '😺 Кошелек бота', callback_data: 'my_wallet' }],
+    [{ text: '🔙 Назад', callback_data: 'back_to_main_menu' }],
   ],
 }
 
@@ -121,11 +121,11 @@ export const USER_SETTINGS_MENU = (botStatus: HandiCatStatus): InlineKeyboardMar
     inline_keyboard: [
       [
         {
-          text: `${botStatus === 'ACTIVE' ? '⏸️ Pause Bot' : '▶️ Resume Bot'}`,
+          text: `${botStatus === 'ACTIVE' ? '⏸️ Пауза бота' : '▶️ Возобновить бота'}`,
           callback_data: 'pause-resume-bot',
         },
       ],
-      [{ text: '🔙 Back', callback_data: 'back_to_main_menu' }],
+      [{ text: '🔙 Назад', callback_data: 'back_to_main_menu' }],
     ],
   }
 }
@@ -134,11 +134,11 @@ export const USER_WALLET_SUB_MENU: InlineKeyboardMarkup = {
   inline_keyboard: [
     [
       {
-        text: '🔑 Show private key',
+        text: '🔑 Показать приватный ключ',
         callback_data: 'show_private_key',
       },
     ],
-    [{ text: '🔙 Back', callback_data: 'back_to_main_menu' }],
+    [{ text: '🔙 Назад', callback_data: 'back_to_main_menu' }],
   ],
 }
 
@@ -146,10 +146,10 @@ export const GROUPS_MENU: InlineKeyboardMarkup = {
   inline_keyboard: [
     [
       {
-        text: '🗑️ Delete Group',
+        text: '🗑️ Удалить группу',
         callback_data: 'delete_group',
       },
     ],
-    [{ text: '🔙 Back', callback_data: 'back_to_main_menu' }],
+    [{ text: '🔙 Назад', callback_data: 'back_to_main_menu' }],
   ],
 }
